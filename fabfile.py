8db4a118cd5e352a10579093048957aeb0477e27
Daniele Tricoli <eriol@mornie.org>
2014-01-21 14:09:09 +0100

    Added supertab

--- fabfile.py
+++ fabfile.py
@@ -21,6 +21,7 @@ git_bundles = [
     'https://github.com/SirVer/ultisnips.git',
     'https://github.com/davidhalter/jedi-vim.git',
     'https://github.com/elzr/vim-json.git',
+    'https://github.com/ervandew/supertab.git'
     'https://github.com/godlygeek/tabular.git',
     'https://github.com/jmcantrell/vim-virtualenv.git',
     'https://github.com/kien/ctrlp.vim.git',
